d58d30ad15d39cbf431bfb613cd7c5e9bf891f1a
William Berry <76190311+wberry86@users.noreply.github.com>
2021-07-25 17:59:38 -0500

    updated readme and css styling as well as quiz functionality

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# code-quiz
-A coding challenge that focuses on web API's through the process of building a timed quiz.
+# Code-Quiz
+
+
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-Apache-blue)](https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [Link to Website](#link)
+  - [Screenshot](#screenshot)
+  ## Description:
+  This challenge consists of building a timed coding quiz with multiple choice questions. This quiz will feature dynamically updated HTML and CSS powered by javascript.   
+  ## Installation:
+  none
+  ## Usage:
+  click on link and open in browser
+  ## License:
+  Apache
+  [Github License](Apache)
+  ## Contributing:
+  undefined
+  ## Tests:
+  undefined
+  ## Questions:
+  Below are links to my Github page and email in case you would like to contact me or have any further questions.
+  ## Link
+  [Link to Code Quiz](https://wberry86.github.io/code-quiz/)
+  ## Screenshot
+  ![Portfolio screenshot](https://github.com/wberry86/react-portfolio/blob/main/src/assets/project/images/portfolio.PNG)
+
+  - Github: [wberry86](https://github.com/wberry86)
+  - Email: wberry86@gmail.com
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,59 @@
 var startButton = document.getElementById("start");
-var introText = document.getElementById("introText-container");
-var questionBox = document.getElementById("question-container");
+var introText = document.getElementById("introTextContainer");
+var answerButtons = document.getElementById("answer-buttons");
 var timer = document.getElementById("timer-container");
-var infoEl = document.createElement("div");
-var imgEl = document.createElement("img");
-var questionEl = document.createElement("div");
-var buttonA = document.getElementById("1");
-var buttonB = document.getElementById("2");
-var buttonC = document.getElementById("3");
+
+var btnA = document.getElementById("a");
+var btnB = document.getElementById("b");
+var btnC = document.getElementById("c");
 
 
+var currentQuestion = 0;
+var i = 0;
 
+var myQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "ESLint",
+      b: "jQuery",
+      c: "RequireJS",
+      
+    },
+    correctAnswer: "a",
+  },
+];
 
-
-
+// when start button is pressed a new <li> is produced in <section><section/>
 var questionEl = document.querySelector("#tasks-to-do");
 
-startButton.addEventListener("click", function() {
+var questionHandler = function(event) {
   var questionItemEl = document.createElement("li");
   questionItemEl.className = "task-item";
-  questionItemEl.textContent = "This is a new question.";
+  questionItemEl.textContent = myQuestions[currentQuestion].question;
   questionEl.appendChild(questionItemEl);
-});
+};
 
+startButton.addEventListener("click", questionHandler);
 
-
-
-
-
-
-// make sure to hide questions at start
-
-// on click make questions appear and intro text to disapear
-// timer also starts on click
-// first question appears
 
 
 // START BUTTON
@@ -43,63 +63,159 @@ startButton.addEventListener("click", function () {
   countdown();
   startGame();
 });
-/*
-// if answered correctly move to next question
-// else alert try again
-if (answer is correct) {
-    nextQuestion()
-    } else {
-        alert("wrong,select again")
-    } 
-*/
+
+
+
+// give classname that specifys whether that answer is correct or incorrenct, then onButtonClick di if statement that checks className if selected choice === correct answer is correct else wrong answer
+
+//compare id value to correct answer
+
+
+
+// on button click find value of buttons id then store that value in a variable then console.log it
+var buttonListenerA = document.getElementById("a").addEventListener("click", function() {
+  console.log(btnA.id)
+  if (btnA.id === myQuestions[currentQuestion].correctAnswer) {
+    window.alert("Correct!");
+    nextQuestion();
+  } else {
+    window.alert("Wrong answer");
+  }
+});
+
+var buttonListenerB = document.getElementById("b").addEventListener("click", function() {
+  console.log(btnB.id)
+  if (btnB.id === myQuestions[currentQuestion].correctAnswer) {
+    window.alert("Correct!");
+    nextQuestion();
+  } else {
+    window.alert("Wrong answer");
+  }
+});
+
+var buttonListenerC = document.getElementById("c").addEventListener("click", function() {
+  console.log(btnC.id)
+  if (btnC.id === myQuestions[currentQuestion].correctAnswer) {
+    window.alert("Correct!");
+    nextQuestion();
+  } else {
+    window.alert("Wrong answer");
+  }
+});
+
+function nextQuestion() {
+  currentQuestion++;
+  questionHandler();
+}
+
+function endGame() {
+  if (currentQuestion === 3) {
+    console.log("Congratulations! you have reached the end of the game");
+  }
+}
+
+
+//compare that value to the value of the correct answer
+
+// function checkAnswer() {
+//   if (answerChosen === this.myQuestions[i].correctAnswer) {
+//     window.alert("You answered correctly")
+//   } else {
+//     window.alert("incorrect")
+//   }
+// }
+
+//for each question isolate the correct answer then look for what the user pressed
+// do with if statement or switch case
+
+
+
+
+
+
+
+
+
+
+
 function startGame() {
-  document.getElementById("questionField").textContent = myQuestions[currentQuestion].question;
-  buttonA.innerHTML = myQuestions[currentQuestion].answers.a;
-  buttonB.innerHTML = myQuestions[currentQuestion].answers.b;
-  buttonC.innerHTML = myQuestions[currentQuestion].answers.c;
   
-  var questionTextBox = document.querySelector(".currentQuestionTextBox");
+  btnA.innerHTML = myQuestions[currentQuestion].answers.a;
+  btnB.innerHTML = myQuestions[currentQuestion].answers.b;
+  btnC.innerHTML = myQuestions[currentQuestion].answers.c;
+
+  
   if (currentQuestion === 1) {
-      questionTextBox.textContent = myQuestions[0].question;
-  }
-  else if (currentQuestion === 2) {
-      questionTextBox.textContent = myQuestions[1].question;
+    questionTextBox.textContent = myQuestions[0].question;
+  } else if (currentQuestion === 2) {
+    questionTextBox.textContent = myQuestions[1].question;
   } else if (currentQuestion === 3) {
-      questionTextBox.textContent = myQuestions[2].question;
+    questionTextBox.textContent = myQuestions[2].question;
   }
-  
-  }
-
-  buttonC.addEventListener("click", function () {
-    // if statement determing action according to answer
-    var correctAnswer = myQuestions[currentQuestion].correctAnswer;
-    
-  });
-
-
+}
 
 // funtion designed to make questions appear on the html webpage after start button is clicked
 function showQuestion() {
-
-    questionBox.style.display = "block";
-    timer.style.display = "block";
-
-    
-
-  
-  
+  answerButtons.style.display = "block";
+  timer.style.display = "block";
 }
-
 
 // function designed to make start button and intro text dissapear after start button is clicked
 function closeDiv() {
   introText.style.display = "none";
   startButton.style.display = "none";
-  
 }
 
-// Timer function
-//set minutes
+
+
+
+
+
+
+
+
+// var infoEl = document.createElement("div");
+// var imgEl = document.createElement("img");
+//var questionEl = document.createElement("div");
+
+// function nextQuestion() {}
+// // using a for loop update questionTextBox.textContent with currentQuestion which comes from the myQuestions array
+// // use for each here:
+
+
+// function showDiv() {
+//     questionBox.style.display = "block";
+//   }
+
+// function startTimer() {
+//   // set interval timer
+//   // what is left on timer = the score
+//   // wrong answer is time subtracted
+// }
+
+// function gameOver() {
+//   setInterval(function () {
+//     alert("Times Up - Game Over!");
+//   }, 30000);
+// } 
+
+
+// // add question attempt 1
+// questionEl.textContent =
+//     "myQuestions[0]";
+ 
+//   infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+  
+//   questionEl.setAttribute("style", "font-size:25px; text-align:center");
+//   document.body.appendChild(infoEl);
+//   infoEl.appendChild(imgEl);
+//   infoEl.appendChild(questionEl);
+  
+  
+
+
+
+
 var mins = 2;
 
 //calculate the seconds
@@ -160,77 +276,4 @@ function getseconds() {
 
 
 
-var myQuestions = [
-  {
-    question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
-    },
-    correctAnswer: "d",
-  },
-];
-
-var currentQuestion = 0;
-var i = 0;
-
-
-
-function nextQuestion() {
-    
-}
-// using a for loop update questionTextBox.textContent with currentQuestion which comes from the myQuestions array
-// use for each here:
-
-
-/*
-function showDiv() {
-    questionBox.style.display = "block";
-  }
-
-function startTimer() {
-  // set interval timer
-  // what is left on timer = the score
-  // wrong answer is time subtracted
-}
-
-function gameOver() {
-  setInterval(function () {
-    alert("Times Up - Game Over!");
-  }, 30000);
-} 
-
-
-// add question attempt 1
-questionEl.textContent =
-    "myQuestions[0]";
- 
-  infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-  
-  questionEl.setAttribute("style", "font-size:25px; text-align:center");
-  document.body.appendChild(infoEl);
-  infoEl.appendChild(imgEl);
-  infoEl.appendChild(questionEl);
-  
-  */
 
